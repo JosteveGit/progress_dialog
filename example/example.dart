511772a468ee171f920227aff5c90f84c64b4392
JosteveGit <adekanbijosteve@gmail.com>
2020-04-11 22:22:54 +0100

    Example Maintainer Description Made Longer

---
+++ example/example.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:simpleprogressdialog/progressdialog.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () {
+          ProgressDialog dialog = ProgressDialog(context: context)
+            ..show(
+                message: "Please wait...",
+                title: "Fetching Data",
+                centerTile: true,
+                titleStyle: TextStyle(fontSize: 40, color: Colors.orange[900]),
+                messageStyle: TextStyle(fontSize: 20, color: Colors.green));
+          Timer(Duration(seconds: 10), () {
+            dialog.dismiss();
+          });
+        },
+      ),
+    );
+  }
+}
